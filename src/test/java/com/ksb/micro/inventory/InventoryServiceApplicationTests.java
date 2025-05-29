703951df055600e6a18de7826fe33ae8076b90f6
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-05-29 22:25:28 +0530

    init from IDE

---
+++ src/test/java/com/ksb/micro/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksb.micro.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
