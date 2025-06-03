eadf08042163b40cc2983cf2e78d46e86ed8d31b
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-06-03 19:59:46 +0530

    swagger api-config --added

--- src/main/java/com/ksb/micro/inventory/config/OpenAPIConfig.java
+++ src/main/java/com/ksb/micro/inventory/config/OpenAPIConfig.java
@@ -1,2 +1,19 @@
-package com.ksb.micro.inventory.config;public class OpenAPIConfig {
+package com.ksb.micro.inventory.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI inventoryServiceAPI(){ //documentation to describe your api service
+        return new OpenAPI()
+                .info(new Info().title("Inventory Service API")
+                        .description("This is the REST API for Inventory Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")));
+    }
 }
